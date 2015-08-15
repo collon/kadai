e9b1cb2fd2aaaa5a64560fcfd4b6cceaf8ef6288
miu <miu@miu-no-MacBook.local>
2015-08-16 03:21:24 +0900

    requirejs and module

--- WebContent/app/index.js
+++ WebContent/app/index.js
@@ -1,12 +1,28 @@
+requirejs.config({
+	paths: {
+		'jquery': 'lib/jquery-2.1.4.min',
+		'jquery.bootstrap': 'lib/bootstrap.min',
+		'text': 'lib/text',
+		'domReady': 'lib/domReady',
+		'i18n': 'lib/i18n',
+		'js': 'js',
+		'html': 'html'
+	},
+	shim: {
+		'jquery.bootstrap': {
+			deps: ['jquery']
+		}
+	}
+});
+
 require([
-	
 ], function () {
-	// ほんとはいろいろやりたい
-	// ログイン画面を表示するとか、ログインに成功してからmain画面を表示するとか
-	
-	// とりあえず、すぐにmain画面を呼び出す
-	require([
-		"app/main"
-	], function (main) {
-	});
+	// ほんとは、ここでいろいろとinitializeな処理が必要、なはず
+	// ブラウザーチェックとか？
+	// その他、このアプリを使用できない場合のチェック処理
+
+	// main画面読み込み (main画面に関する処理は、main.jsで実装する)
+	// 注: 本来は、ここでログイン画面を読み込む。
+	//     ログイン画面でログインに成功したら、main画面を読み込む
+	require(['js/main']);
 });
---
+++ src/kadai/servlet/MiuTestServlet.java
@@ -0,0 +1,88 @@
+package kadai.servlet;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Servlet implementation class MiuTestServlet
+ */
+@WebServlet("/MiuTestServlet")
+public class MiuTestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	private static class GetUserInfoRequest {
+		public List<String> arrayUserId = null;
+	}
+	private class MiuDataResponse {
+		public String name = null;
+		public int score = 0;
+		public List<String> yaku = new ArrayList<String>();
+	}
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MiuTestServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		// 本当はまず、セッションチェック（ログインチェック）
+		
+		// カスタムヘッダーからアクション名を取得
+		String strAction = request.getHeader("x-custom-action-id");
+		
+		if (strAction == null) {
+			// アクション名がない：異常
+			return;
+		}
+		
+		BufferedReader bufferReaderBody = new BufferedReader(request.getReader());
+		String body = bufferReaderBody.readLine();
+		
+		ObjectMapper mapper = new ObjectMapper();
+		
+		switch (strAction) {
+		case "getUserInfo":
+			GetUserInfoRequest requestParam = mapper.readValue(body, GetUserInfoRequest.class);
+			System.out.println(requestParam.arrayUserId.toString());
+			break;
+		}
+		
+		MiuDataResponse res = new MiuDataResponse();
+		res.name = "miura";
+		res.score = 123;
+		res.yaku.add("neko");
+		res.yaku.add("melon");
+		response.setContentType("application/json;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.print(mapper.writeValueAsString(res));
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
